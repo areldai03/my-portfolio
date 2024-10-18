734e3921f73c3a01ebf86f0a11e93e231e20c234
areldai03 <125785005+areldai03@users.noreply.github.com>
2024-10-18 18:09:57 +0900

    Update ProfileIcon.tsx

--- app/app/components/ProfileIcon.tsx
+++ app/app/components/ProfileIcon.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 const ProfileIcon = () => {
     return (
         <div className="flex flex-col items-center p-4 bg-gray-100">
-            <img
+            <Image
                 src="/images/doggy.png"
                 alt="Picture of the author"
+                width={500}
+                height={500}
                 className="rounded-full w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 object-cover transform transition duration-300 hover:scale-105 hover:shadow-2xl" 
             />
             <h2 className="mt-4 text-xl md:text-2xl font-semibold">Daisuke Maekawa</h2>
